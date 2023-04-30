ae8ddd82c483108c8c173a4dafda5a93a88031d9
vedy22 <89541794+vedy22@users.noreply.github.com>
2023-05-01 00:49:05 +0300

    Update views.py

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,17 +1,21 @@
+from http import HTTPStatus
+
 from django.shortcuts import render
 
 
-def page_not_found(request, exception):
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
-
-
-def permission_denied(request, exception):
-    return render(request, 'core/403.html', {'path': request.path}, status=403)
-
-
 def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
+    template = 'core/403csrf.html'
+    return render(request, template)
+
+
+def page_not_found(request, exception):
+    template = 'core/404.html'
+    return render(request,
+                  template,
+                  {'path': request.path},
+                  status=HTTPStatus.NOT_FOUND)
 
 
 def server_error(request):
-    return render(request, 'core/500.html', {'path': request.path}, status=500)
+    template = 'core/500.html'
+    return render(request, template, status=HTTPStatus.INTERNAL_SERVER_ERROR)
